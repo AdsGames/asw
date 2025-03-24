0834a87d24df1b5199ae5043b9cf25fe9e93445c
Allan Legemaate <alegemaate@gmail.com>
2025-03-24 14:03:25 -0400

    feat: random module

--- include/asw/asw.h
+++ include/asw/asw.h
@@ -10,6 +10,7 @@
 #include "./modules/input.h"
 #include "./modules/input_keys.h"
 #include "./modules/input_mouse_buttons.h"
+#include "./modules/random.h"
 #include "./modules/sound.h"
 #include "./modules/types.h"
 #include "./modules/util.h"
