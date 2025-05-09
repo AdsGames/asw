3baba79feb8c2fa3786b584f67322de34579d885
Allan Legemaate <alegemaate@gmail.com>
2025-05-08 21:26:26 -0400

    feat: add vec3

--- include/asw/modules/geometry.h
+++ include/asw/modules/geometry.h
@@ -184,6 +184,183 @@ namespace asw {
     T y{0};
   };
 
+  /// @brief A 3D vector in space.
+  ///
+  /// @details This class represents a 3D vector in space. It is used to
+  /// represent points, directions, and sizes.
+  ///
+  template <typename T>
+  class Vec3 {
+   public:
+    /// @brief Default constructor for the Vec3 class.
+    ///
+    Vec3() : x(0), y(0), z(0) {}
+
+    /// @brief Constructor for the Vec3 class.
+    ///
+    /// @param x The x component of the vector.
+    /// @param y The y component of the vector.
+    /// @param z The z component of the vector.
+    ///
+    Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
+
+    /// @brief Get angle between two vectors (in 3D space).
+    ///
+    /// @return The angle of the vector in radians.
+    ///
+    T angle(const Vec3& other) const {
+      T dotProduct = dot(other);
+      T magnitudes = magnitude() * other.magnitude();
+      return std::acos(dotProduct / magnitudes);
+    }
+
+    /// @brief Get distance between two vectors.
+    ///
+    /// @return The distance between the vectors.
+    ///
+    T distance(const Vec3& other) const {
+      return std::sqrt((x - other.x) * (x - other.x) +
+                       (y - other.y) * (y - other.y) +
+                       (z - other.z) * (z - other.z));
+    }
+
+    /// @brief Calculate the dot product of two vectors.
+    ///
+    /// @param other The vector to dot with.
+    /// @return T The dot product of the vectors.
+    ///
+    T dot(const Vec3& other) const {
+      return (x * other.x) + (y * other.y) + (z * other.z);
+    }
+
+    /// @brief Calculate the cross product of two vectors.
+    ///
+    /// @param other The vector to cross with.
+    /// @return Vec3 The cross product of the vectors.
+    ///
+    Vec3 cross(const Vec3& other) const {
+      return Vec3((y * other.z) - (z * other.y), (z * other.x) - (x * other.z),
+                  (x * other.y) - (y * other.x));
+    }
+
+    /// @brief Calculate the magnitude of the vector.
+    ///
+    /// @return T The magnitude of the vector.
+    ///
+    T magnitude() const { return std::sqrt((x * x) + (y * y) + (z * z)); }
+
+    /// @brief Addition operator for the Vec3 class.
+    ///
+    /// @param other The vector to add.
+    /// @return Vec3 The sum of the vectors.
+    ///
+    Vec3 operator+(const Vec3& other) const {
+      return Vec3(x + other.x, y + other.y, z + other.z);
+    }
+
+    /// @brief Subtraction operator for the Vec3 class.
+    ///
+    /// @param other The vector to subtract.
+    /// @return Vec3 The difference of the vectors.
+    ///
+    Vec3 operator-(const Vec3& other) const {
+      return Vec3(x - other.x, y - other.y, z - other.z);
+    }
+
+    /// @brief Multiplication operator for the Vec3 class.
+    ///
+    /// @param scalar The scalar to multiply by.
+    /// @return Vec3 The scaled vector.
+    ///
+    Vec3 operator*(const T scalar) const {
+      return Vec3(x * scalar, y * scalar, z * scalar);
+    }
+
+    /// @brief Division operator for the Vec3 class.
+    ///
+    /// @param scalar The scalar to divide by.
+    /// @return Vec3 The scaled vector.
+    ///
+    Vec3 operator/(const T scalar) const {
+      return Vec3(x / scalar, y / scalar, z / scalar);
+    }
+
+    /// @brief Addition assignment operator for the Vec3 class.
+    ///
+    /// @param other The vector to add.
+    /// @return Vec3& The sum of the vectors.
+    ///
+    Vec3& operator+=(const Vec3& other) {
+      x += other.x;
+      y += other.y;
+      z += other.z;
+      return *this;
+    }
+
+    /// @brief Subtraction assignment operator for the Vec3 class.
+    ///
+    /// @param other The vector to subtract.
+    /// @return Vec3& The difference of the vectors.
+    ///
+    Vec3& operator-=(const Vec3& other) {
+      x -= other.x;
+      y -= other.y;
+      z -= other.z;
+      return *this;
+    }
+
+    /// @brief Multiplication assignment operator for the Vec3 class.
+    ///
+    /// @param scalar The scalar to multiply by.
+    /// @return Vec3& The scaled vector.
+    ///
+    Vec3& operator*=(const T scalar) {
+      x *= scalar;
+      y *= scalar;
+      z *= scalar;
+      return *this;
+    }
+
+    /// @brief Division assignment operator for the Vec3 class.
+    ///
+    /// @param scalar The scalar to divide by.
+    /// @return Vec3& The scaled vector.
+    ///
+    Vec3& operator/=(const T scalar) {
+      x /= scalar;
+      y /= scalar;
+      z /= scalar;
+      return *this;
+    }
+
+    /// @brief Equality operator for the Vec3 class.
+    ///
+    /// @param other The vector to compare.
+    /// @return bool True if the vectors are equal.
+    ///
+    bool operator==(const Vec3& other) const {
+      return x == other.x && y == other.y && z == other.z;
+    }
+
+    /// @brief Inequality operator for the Vec3 class.
+    ///
+    /// @param other The vector to compare.
+    /// @return bool True if the vectors are not equal.
+    ///
+    bool operator!=(const Vec3& other) const {
+      return x != other.x || y != other.y || z != other.z;
+    }
+
+    /// @brief The x component of the vector.
+    T x{0};
+
+    /// @brief The y component of the vector.
+    T y{0};
+
+    /// @brief The z component of the vector.
+    T z{0};
+  };
+
   /// @brief A 2D rectangle in space.
   ///
   /// @details This class represents a 2D rectangle in space. It is used to
